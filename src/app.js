3379be99b96a738c9d4556f4d261b5d2ac22709a
Franco Alvarez Muñoz <falvarez.munoz@hotmail.es>
2024-07-24 18:47:14 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,27 +1,25 @@
+//declarando las listas de los elementos necesarios
 let who = ["The dog", "My grandma", "The mailman", "My bird"];
 let action = ["ate", "peed", "crushed", "broke"];
 let what = ["my homework", "my phone", "the car"];
 let when = ["before the class", "when I was sleeping", "while I was exercising", "during my lunch", "while I was praying"];
 
-function generateAnExcuse(array1, array2, array3, array4) {
-  let newExcuse = "";
-  newExcuse = newExcuse.concat(
-    array1[generateNumber(array1)],
-    " ",
-    array2[generateNumber(array2)],
-    " ",
-    array3[generateNumber(array3)],
-    " ",
-    array4[generateNumber(array4)]
-  );
-  return newExcuse;
-}
-
+//funcion que genera un numero aleatorio para seleccionar un index al azar de cada elemento
 function generateNumber(anArray) {
   let num = Math.floor(Math.random() * anArray.length);
   return num;
 }
 
-window, addEventListener("load", loaded());
+//funcion que concatena cada elemento para formar la excusa
+function generateAnExcuse(array1, array2, array3, array4) {
+  let newExcuse = "";
+  newExcuse = newExcuse.concat(array1[generateNumber(array1)]," ",array2[generateNumber(array2)]," ",array3[generateNumber(array3)]," ",array4[generateNumber(array4)]);
+  return newExcuse;
+}
 
+//funcion que reemplaza el parrafo #excuse por la nueva excusa generada
 const loaded = () => {document.getElementById("excuse").innerHTML = generateAnExcuse(who, action, what, when);};
+
+//llama a la funcion loaded al finalizar de cargar la pagina
+window.addEventListener("load", loaded());
+
