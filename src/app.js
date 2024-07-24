25baadeeda558709e71289074643134d2861ad94
Franco Alvarez Muñoz <falvarez.munoz@hotmail.es>
2024-07-24 18:33:17 +0000

    24-07 changes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,31 @@
-var emptyExcuse = {
-  elements: [who, action, what, when]
-};
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
 
-let who = ['The dog','My grandma','The mailman','My bird'];
-let action = ['ate','peed','crushed','broke'];
-let what = ['my homework','my phone','the car'];
-let when = ['before the class','when I was sleeping','while I was exercising','during my lunch','while I was praying'];
+function generateAnExcuse(array1, array2, array3, array4) {
+  let newExcuse = "";
+  newExcuse = newExcuse.concat(
+    array1[generateNumber(array1)],
+    " ",
+    array2[generateNumber(array2)],
+    " ",
+    array3[generateNumber(array3)],
+    " ",
+    array4[generateNumber(array4)]
+  );
+  return newExcuse;
+}
+
+function generateNumber(anArray) {
+  let num = Math.floor(Math.random() * anArray.length);
+  return num;
+}
+
+// console.log(generateAnExcuse(who,action,what,when));
