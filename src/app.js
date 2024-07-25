03992ba5ad5610a29c90dafbc19028cb52b00eef
Franco Alvarez Muñoz <falvarez.munoz@hotmail.es>
2024-07-25 03:19:16 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -2,7 +2,13 @@
 let who = ["The dog", "My grandma", "The mailman", "My bird"];
 let action = ["ate", "peed", "crushed", "broke"];
 let what = ["my homework", "my phone", "the car"];
-let when = ["before the class", "when I was sleeping", "while I was exercising", "during my lunch", "while I was praying"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
 
 //funcion que genera un numero aleatorio para seleccionar un index al azar de cada elemento
 function generateNumber(anArray) {
@@ -13,13 +19,25 @@ function generateNumber(anArray) {
 //funcion que concatena cada elemento para formar la excusa
 function generateAnExcuse(array1, array2, array3, array4) {
   let newExcuse = "";
-  newExcuse = newExcuse.concat(array1[generateNumber(array1)]," ",array2[generateNumber(array2)]," ",array3[generateNumber(array3)]," ",array4[generateNumber(array4)]);
+  newExcuse = newExcuse.concat(
+    array1[generateNumber(array1)],
+    " ",
+    array2[generateNumber(array2)],
+    " ",
+    array3[generateNumber(array3)],
+    " ",
+    array4[generateNumber(array4)]
+  );
   return newExcuse;
 }
 
-//funcion que reemplaza el parrafo #excuse por la nueva excusa generada
-const loaded = () => {document.getElementById("excuse").innerHTML = generateAnExcuse(who, action, what, when);};
-
-//llama a la funcion loaded al finalizar de cargar la pagina
-window.addEventListener("load", loaded());
-
+/* funcion que reemplaza el parrafo #excuse por la nueva excusa generada,
+investigando durante un buen tiempo descubrí que anteponiendo "window." me permitía poder acceder a la función directamente sin necesidad de agregar la función en html*/
+window.loaded = () => {
+  document.getElementById("excuse").innerHTML = generateAnExcuse(
+    who,
+    action,
+    what,
+    when
+  );
+};
